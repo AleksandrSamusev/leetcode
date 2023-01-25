694329ab9da781a4a4dfc83aa778d3e0bd253ab9
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-25 17:43:28 +0300

    init commit

---
+++ Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        var result = 0;
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        if(nums[0] != nums[1]) {
+            result = nums[0];
+        }
+        for(int i = 1; i<nums.length-2; i++) {
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
+                result = nums[i];
+            }
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]) {
+            result = nums[nums.length-1];
+        }
+        return result;
+    }
+}
