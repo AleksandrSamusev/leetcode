ba49f0d46d58371da4a66da2db8f57610dc493a6
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-25 20:45:53 +0300

    168_excel_sheet_column_number-DONE

--- Solution.java
+++ Solution.java
@@ -1,18 +1,13 @@
 //168_excel_sheet_column_number
 public class Solution {
     public static void main(String[] args) {
-
-        System.out.println(convertToTitle(100));
+        System.out.println(convertToTitle(1000));
     }
-
     public static String convertToTitle(int columnNumber) {
-        final StringBuilder sb = new StringBuilder();
-
+        StringBuilder sb = new StringBuilder();
         int num = columnNumber - 1;
         while (num >= 0) {
-            int numChar = (num % 26) + 65;
-            System.out.println(num % 26);
-            sb.append((char) numChar);
+            sb.append((char) ((num % 26) + 65));
             num = (num / 26) - 1;
         }
         return sb.reverse().toString();
