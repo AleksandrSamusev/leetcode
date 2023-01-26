e27d23cc6a0e2bb0febaf0bc32e6f641cc1b4d02
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-26 17:48:24 +0300

    202_Happy_number_DONE

--- Solution.java
+++ Solution.java
@@ -1,26 +1,30 @@
-import java.util.Arrays;
+//202. Happy number
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+
+        System.out.println(isHappy(4));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNextValue(n);
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
+        return n == 1;
+    }
+
+    private static int getNextValue(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int d = num % 10;
+            num = num / 10;
+            sum += d * d;
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return sum;
     }
 }
