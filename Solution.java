ca6a8a0d10984ba5f84b7629dac82ced87ac9282
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-31 22:29:25 +0300

    219_Contains_Duplicate_2-done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
+//219_Contains_Duplicate_2
+
+import java.util.HashMap;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {1, 2, 3, 1, 2, 3};
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(nums, k));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(map.get(nums[i]) - i) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return false;
     }
 }
