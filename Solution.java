e59c30057a2183cc498916b4d01eca3d5755bbce
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-31 19:10:14 +0300

    217_contains_duplicate-done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,19 @@
+//217_contains_duplicate
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {1, 2, 3};
+        System.out.println(containsDuplicate(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
+    public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return false;
     }
 }
