325d13f25a2f34727ec53eda7664cad8c3e099f1
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-01-31 18:42:26 +0300

    1528_Shuffle_String-done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,17 @@
-import java.util.Arrays;
-
+//1528_shuffle_string
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {4, 5, 6, 7, 0, 2, 1, 3};
+        String s = "codeleet";
+        System.out.println(restoreString(s, nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static String restoreString(String s, int[] indices) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        for (int i = 0; i < indices.length; i++) {
+            sb.setCharAt(indices[i], s.charAt(i));
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return sb.toString();
     }
 }
