34685686eb6a6ab09e189ff1346a223a8e9e2198
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-02 09:52:03 +0300

    709. To Lower Case - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,13 @@
-import java.util.Arrays;
+//709. To Lower Case
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+    public static String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        return (sb.append(s.toLowerCase())).toString();
     }
 }
