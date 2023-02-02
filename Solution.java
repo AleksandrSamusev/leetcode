bef678a4941813a2185cfbe5d398ba5d82ffff69
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-02 14:15:42 +0300

    1071. Greatest Common Divisor of Strings - init

--- Solution.java
+++ Solution.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
+//1071. Greatest Common Divisor of Strings
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+
+        System.out.println(gcdOfStrings(str1, str2));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static String gcdOfStrings(String str1, String str2) {
+        String newStr = "";
+        if (!str1.contains(str2) && !str2.contains(str1)) {
+            return newStr;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        if (str1.length() > str2.length()) {
+            if (str1.contains(str2) && (str1.replace(str2, "").equals("") || str1.replace(str2, "").equals(str2))) {
+                newStr = str1.replaceFirst(str2, "");
+            }
+        } else {
+            if (str2.contains(str1)) {
+                newStr = str2.replaceFirst(str1, "");
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return newStr;
     }
 }
