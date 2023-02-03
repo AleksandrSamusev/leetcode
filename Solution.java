e35113116020226aa8fc72f89e916f0760e0cc35
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-04 01:16:25 +0300

    917. Reverse Only Letters - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,36 @@
-import java.util.Arrays;
+//917. Reverse Only Letters
+
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "Czyr^";
+        System.out.println(reverseOnlyLetters(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static String reverseOnlyLetters(String s) {
+        ArrayList<String> list = new ArrayList<>();
+        String[] array = new String[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (((int) s.charAt(i) <= 122 && s.charAt(i) >= 97)
+                    || ((int) s.charAt(i) <= 90 && (int) s.charAt(i) >= 65)) {
+                list.add(String.valueOf(s.charAt(i)));
+            } else {
+                array[i] = String.valueOf(s.charAt(i));
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        int temp = 0;
+        for (int j = array.length - 1; j >= 0; j--) {
+            if (array[j] == null) {
+                array[j] = list.get(temp);
+                temp++;
+            }
         }
-        return result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+        }
+        return sb.toString();
     }
 }
