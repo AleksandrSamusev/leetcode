30f10ae4915e27dbf83cf6038f5c8fd7bc39eb11
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-06 02:50:33 +0300

    1108. Defanging an IP Address - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,12 @@
-import java.util.Arrays;
+//1108. Defanging an IP Address
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String address = "255.100.50.0";
+        System.out.println(defangIPaddr(address));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+    public static String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
     }
 }
