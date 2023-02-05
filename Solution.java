b91c8e310f8083c1e8e84408228b57142cd126df
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-06 02:05:02 +0300

    771. Jewels and Stones - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,21 @@
-import java.util.Arrays;
+//771. Jewels and Stones
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String jewels = "z";
+        String stones = "ZZ";
+        System.out.println(numJewelsInStones(jewels, stones));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            for (int j = 0; j < stones.length(); j++) {
+                if (stones.charAt(j) == jewels.charAt(i)) {
+                    count++;
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return count;
     }
 }
