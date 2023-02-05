16dcb0036d3a543827c9fd67a22f02a959ba57ac
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-06 01:00:21 +0300

    748. Shortest Completing Word - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,51 @@
-import java.util.Arrays;
+//748. Shortest Completing Word
+
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String licensePlate = "1s3 PSt";
+        String[] words = {"step", "steps", "stripe", "stepple"};
+        System.out.println(shortestCompletingWord(licensePlate, words));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static String shortestCompletingWord(String licensePlate, String[] words) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < licensePlate.length(); i++) {
+            if (Character.isLetter(licensePlate.charAt(i))) {
+                sb.append(String.valueOf(licensePlate.charAt(i)).toLowerCase());
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        for (String str : words) {
+            if (isContain(sb, str)) {
+                list.add(str);
+            }
+        }
+        String result = "";
+        int length = Integer.MAX_VALUE;
+        for (String str : list) {
+            if (str.length() < length) {
+                length = str.length();
+                result = str;
+            }
         }
         return result;
+    }
+
+    public static boolean isContain(StringBuilder sb, String check) {
+        StringBuilder local = new StringBuilder(sb);
+        for (int i = 0; i < check.length(); i++) {
+            for (int j = 0; j < local.length(); j++) {
+                if (check.charAt(i) == (local.charAt(j))) {
+                    local.deleteCharAt(j);
+                    if (local.length() == 0) {
+                        return true;
+                    }
+                    break;
+                }
+            }
+        }
+        return false;
     }
 }
