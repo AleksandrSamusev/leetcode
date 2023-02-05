b83d71691cb9b8dad96433e8aa0551b6e256526f
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-06 02:42:41 +0300

    804. Unique Morse Code Words - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,33 @@
-import java.util.Arrays;
+//804. Unique Morse Code Words
+
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String[] words = {"gin", "zen", "gig", "msg"};  //a-z , 97-122
+        System.out.println(uniqueMorseRepresentations(words));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+                "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-",
+                ".--", "-..-", "-.--", "--.."};
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (!list.contains(wordToMorse(morse, words[i]))) {
+                list.add(wordToMorse(morse, words[i]));
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        return list.size();
+    }
+
+    private static String wordToMorse(String[] morse, String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(morse[word.charAt(i) - 97]);
         }
-        return result;
+        return sb.toString();
     }
 }
