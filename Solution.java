c02b61b3b8d340d65a9eb39b2b6e895442a88a44
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-07 00:08:11 +0300

    821. Shortest Distance to a Character - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,49 @@
+//821. Shortest Distance to a Character
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "zzze";
+        char c = 'e';
+        System.out.println(Arrays.toString(shortestToChar(s, c)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static int[] shortestToChar(String s, char c) {
+        int[] array = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            array[i] = theClosestChar(s, i, c);
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        return array;
+    }
+
+    public static int theClosestChar(String s, int currentIndex, char target) {
+        int indexesLeft = 0;
+        int indexesRight = 0;
+        if (s.charAt(currentIndex) == target) {
+            return 0;
+        }
+        if (currentIndex != 0 || currentIndex == s.length() - 1) {
+            for (int i = currentIndex; i >= 0; i--) {
+                if (s.charAt(i) == target) {
+                    indexesLeft = Math.abs(i - currentIndex);
+                    break;
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        if (currentIndex == 0 || currentIndex != s.length() - 1) {
+            for (int i = currentIndex; i < s.length(); i++) {
+                if (s.charAt(i) == target) {
+                    indexesRight = Math.abs(i - currentIndex);
+                    break;
+                }
+            }
         }
-        return result;
+        if (indexesLeft == 0) {
+            return indexesRight;
+        } else if (indexesRight == 0) {
+            return indexesLeft;
+        }
+        return Integer.min(indexesLeft, indexesRight);
     }
 }
