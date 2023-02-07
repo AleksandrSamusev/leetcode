f03d5abe6d83f3f1cdb8eb43ebd8e46c70c40564
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-07 18:01:15 +0300

    342. Power of Four - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,24 @@
-import java.util.Arrays;
+// 342. Power of Four
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int n = 16;
+        System.out.println(isPowerOfFour(n));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static boolean isPowerOfFour(int n) {
+        if (n == 0) {
+            return false;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        if (n == 1 || n == 4) {
+            return true;
+        }
+        while (n % 4 == 0) {
+            n = n / 4;
+            if (n == 4) {
+                return true;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return false;
     }
 }
