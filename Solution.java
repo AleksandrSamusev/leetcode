d79e58ec1a3943dd7f0317f8f475b53310b67c41
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-07 18:25:32 +0300

    344. Reverse String - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,20 @@
-import java.util.Arrays;
+//344. Reverse String
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static void reverseString(char[] s) {
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : s) {
+            sb.append(c);
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
+        sb.reverse();
+        for (int i = 0; i < s.length; i++) {
+            s[i] = sb.charAt(i);
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
     }
 }
