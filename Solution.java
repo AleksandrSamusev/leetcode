1385ed28cfaa242f9e4743fe59aea15be7c663aa
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-10 18:37:33 +0300

    290. Word Pattern - done

--- Solution.java
+++ Solution.java
@@ -1,25 +1,27 @@
+//338. Counting Bits
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int n = 5;
+        System.out.println(Arrays.toString(countBits(n)));
+    }
+    public static int[] countBits(int n) {
+        int[] resultArray = new int[n+1];
+        for(int i = 0; i< resultArray.length; i++) {
+            resultArray[i] = getSum(i);
+        }
+        return resultArray;
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int getSum(int n) {
+        String str = Integer.toBinaryString(n);
+        int result = 0;
+        for(int i = 0; i<str.length(); i++) {
+            if(Integer.parseInt(String.valueOf(str.charAt(i))) == 1) {
+                result++;
             }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
         }
         return result;
     }
