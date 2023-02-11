7ed6355f744e0bc32aa7e24fccd830ab5b2db023
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-11 20:06:10 +0300

    345. Reverse Vowels of a String - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,29 @@
-import java.util.Arrays;
+//345. Reverse Vowels of a String
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(reverseVowels(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : chars) {
+            if (ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117
+                    || ch == 65 || ch == 69 || ch == 73 || ch == 79 || ch == 85) {
+                sb.append(ch);
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        sb.reverse();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 97 || chars[i] == 101 || chars[i] == 105 || chars[i] == 111 || chars[i] == 117
+                    || chars[i] == 65 || chars[i] == 69 || chars[i] == 73
+                    || chars[i] == 79 || chars[i] == 85) {
+                chars[i] = sb.charAt(0);
+                sb.deleteCharAt(0);
+            }
         }
-        return result;
+        return new String(chars);
     }
 }
