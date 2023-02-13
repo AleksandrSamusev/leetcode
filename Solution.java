06bbfc3ff5b52fb6604d8fb099327f4338a55ce9
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-14 00:20:46 +0300

    1523. Count odd numbers in an interval range - done

--- Solution.java
+++ Solution.java
@@ -1,25 +1,18 @@
-import java.util.Arrays;
+//1523. Count odd numbers in an interval range
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int low = 14;
+        int high = 17;
+        System.out.println(countOdds(low, high));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+    public static int countOdds(int low, int high) {
+        int result = 0;
+        if ((low % 2 != 0 && high % 2 != 0) || (low % 2 != 0 && high % 2 == 0) || (low % 2 == 0 && high % 2 != 0)) {
+            result = ((high - low) / 2) + 1;
+        } else {
+            result = (high - low) / 2;
         }
         return result;
     }
