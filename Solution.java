6be4644b13cbe23f9f59603e2faaf941ebff1415
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-17 02:57:17 +0300

    680. Valid Palindrome II - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
+//680. Valid Palindrome II
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "ebcbbececabbacecbbcbe";
+        System.out.println(validPalindrome(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static boolean validPalindrome(String s) {
+        for (int i = 0; i < (s.length() / 2); i++) {
+            if (s.charAt(i) != (s.charAt(s.length() - 1 - i))) {
+                String one = s.substring(i, s.length() - i - 1);
+                String two = s.substring(i + 1, s.length() - i);
+                StringBuilder sb1 = new StringBuilder(one);
+                StringBuilder sb2 = new StringBuilder(two);
+                sb1.reverse();
+                sb2.reverse();
+                if (one.equals(sb1.toString()) || two.equals(sb2.toString())) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return true;
     }
 }
