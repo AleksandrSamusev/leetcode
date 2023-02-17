21aed44cfb08617b80aa15193b76905f203e34c5
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-18 00:49:21 +0300

    905. Sort Array By Parity - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,23 @@
+//905. Sort Array By Parity
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {3, 1, 1, 1, 6, 8, 2, 4};
+        System.out.println(Arrays.toString(sortArrayByParity(nums)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int[] sortArrayByParity(int[] nums) {
+        int pasteIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                int temp = nums[pasteIndex];
+                nums[pasteIndex] = nums[i];
+                nums[i] = temp;
+                pasteIndex++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return nums;
     }
 }
