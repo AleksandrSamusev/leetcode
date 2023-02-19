ba9e02d66cc6d14732f8ccd135d3cf5750df22cc
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-19 21:26:55 +0300

    1089. Duplicate Zeros - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,23 @@
+//1089. Duplicate Zeros
+
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] arr = {1, 0, 2, 3, 0, 4, 5, 0};
+        duplicateZeros(arr);
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 2; j >= i; j--) {
+                    arr[j + 1] = arr[j];
+                }
+                i++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        System.out.println(Arrays.toString(arr));
     }
 }
