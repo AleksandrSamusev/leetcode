97fc17733a26fe17e953bbb3682e4a4a750ff67e
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-26 22:36:23 +0300

    476. Number Complement - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,21 @@
-import java.util.Arrays;
+//476. Number Complement
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int num = 5;
+        System.out.println(findComplement(num));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int findComplement(int num) {
+        String s = Integer.toBinaryString(num);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                sb.append('0');
+            } else {
+                sb.append('1');
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return Integer.parseInt(sb.toString(), 2);
     }
 }
