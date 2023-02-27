44f6a7893cf5dddaab438a1bcde41e475bdb62b7
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-02-27 03:27:16 +0300

    500. Keyboard Row - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,63 @@
+//500. Keyboard Row
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String[] words = {"Hello", "Alaska", "Dad", "Peace"};
+        System.out.println(Arrays.toString(findWords(words)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static String[] findWords(String[] words) {
+
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<Character> top = new ArrayList<>(Arrays.asList('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'));
+        ArrayList<Character> middle = new ArrayList<>(Arrays.asList('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'));
+        ArrayList<Character> bottom = new ArrayList<>(Arrays.asList('z', 'x', 'c', 'v', 'b', 'n', 'm'));
+
+        for (int i = 0; i < words.length; i++) {
+            boolean flag = true;
+            String s = words[i].toLowerCase();
+            char temp = s.charAt(0);
+            if (temp == 'q' || temp == 'w' || temp == 'e' || temp == 'r' || temp == 't' || temp == 'y'
+                    || temp == 'u' || temp == 'i' || temp == 'o' || temp == 'p') {
+
+                for (int k = 1; k < s.length(); k++) {
+                    if (!top.contains(s.charAt(k))) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.add(words[i]);
+                }
+            } else if (temp == 'a' || temp == 's' || temp == 'd' || temp == 'f' || temp == 'g' || temp == 'h'
+                    || temp == 'j' || temp == 'k' || temp == 'l') {
+
+                for (int k = 1; k < s.length(); k++) {
+                    if (!middle.contains(s.charAt(k))) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.add(words[i]);
+                }
+            } else if (temp == 'z' || temp == 'x' || temp == 'c' || temp == 'v' || temp == 'b' || temp == 'n'
+                    || temp == 'm') {
+
+                for (int k = 1; k < s.length(); k++) {
+                    if (!bottom.contains(s.charAt(k))) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.add(words[i]);
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return result.toArray(new String[0]);
     }
 }
