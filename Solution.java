88bdb22ba1c146e6d932e538a56f5bf369a00bcd
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-03 06:09:48 +0300

    747. Largest Number At Least Twice of Others - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,26 @@
+//747. Largest Number At Least Twice of Others
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {3, 6, 1, 0};
+        System.out.println(dominantIndex(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int dominantIndex(int[] nums) {
+        int[] array = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(array);
+        int max = array[array.length - 1];
+        int index = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == max) {
+                index = i;
+            } else if (nums[i] * 2 > max) {
+                return -1;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return index;
     }
 }
