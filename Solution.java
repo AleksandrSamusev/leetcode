87bb6acbad8d99a694d6cfb424c1f3e2c63ddef8
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-04 01:49:07 +0300

    896. Monotonic Array - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,26 @@
-import java.util.Arrays;
+//896. Monotonic Array
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {1, 2, 2, 3};
+        System.out.println(isMonotonic(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static boolean isMonotonic(int[] nums) {
+
+        int decreaseOrEqual = 0;
+        int increaseOrEqual = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                decreaseOrEqual++;
+            } else if (nums[i - 1] < nums[i]) {
+                increaseOrEqual++;
+            } else {
+                decreaseOrEqual++;
+                increaseOrEqual++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return decreaseOrEqual == nums.length - 1 || increaseOrEqual == nums.length - 1;
     }
 }
