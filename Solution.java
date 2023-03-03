3be5eefc808de45f3f7f47061b15afec63508255
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-03 21:45:34 +0300

    704. Binary Search - done (brute force)

--- Solution.java
+++ Solution.java
@@ -1,26 +1,21 @@
-import java.util.Arrays;
+//2529. Maximum Count of Positive Integer and Negative Integer
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {-3, -2, 0, 1, 1, 1, 2};
+        System.out.println(maximumCount(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int maximumCount(int[] nums) {
+        int pos = 0;
+        int neg = 0;
+        for (Integer num : nums) {
+            if (num > 0) {
+                pos++;
+            } else if (num < 0) {
+                neg++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return Math.max(pos, neg);
     }
 }
