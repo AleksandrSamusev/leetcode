80c2acaee36437be4dde52c10e14ffe4f17e7ec1
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-04 03:02:04 +0300

    896. Monotonic Array - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,79 @@
+//888. Fair Candy Swap
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] aliceSizes = {1, 2, 5};
+        int[] bobSizes = {2, 4};
+        System.out.println(Arrays.toString(fairCandySwap(aliceSizes, bobSizes)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int aliceTotalCandies = sumArrayElements(aliceSizes);
+        int bobTotalCandies = sumArrayElements(bobSizes);
+        int diff = Math.abs(bobTotalCandies - aliceTotalCandies);
+        int elDiff = diff / 2;
+
+        int temp = 0;
+
+        if (aliceSizes.length == 1) {
+            if (sumArrayElements(aliceSizes) > sumArrayElements(bobSizes)) {
+                for (int i = 0; i < bobSizes.length; i++) {
+                    if (bobSizes[i] == aliceSizes[0] - elDiff) {
+                        return new int[]{aliceSizes[0], bobSizes[i]};
+                    }
+                }
+            } else {
+                for (int i = 0; i < bobSizes.length; i++) {
+                    if (bobSizes[i] == aliceSizes[0] + elDiff) {
+                        return new int[]{aliceSizes[0], bobSizes[i]};
+                    }
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        if (bobSizes.length == 1) {
+            if (sumArrayElements(bobSizes) > sumArrayElements(aliceSizes)) {
+                for (int i = 0; i < aliceSizes.length; i++) {
+                    if (aliceSizes[i] == bobSizes[0] - elDiff) {
+                        return new int[]{aliceSizes[i], bobSizes[0]};
+                    }
+                }
+            } else {
+                for (int i = 0; i < aliceSizes.length; i++) {
+                    if (aliceSizes[i] == bobSizes[0] + elDiff) {
+                        return new int[]{aliceSizes[i], bobSizes[0]};
+                    }
+                }
+            }
         }
-        return result;
+
+
+        if (aliceTotalCandies > bobTotalCandies) {
+            for (int i = 0; i < aliceSizes.length; i++) {
+                for (int j = 0; j < bobSizes.length; j++) {
+                    if (bobSizes[j] == aliceSizes[i] - elDiff) {
+                        return new int[]{aliceSizes[i], bobSizes[j]};
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < aliceSizes.length; i++) {
+                for (int j = 0; j < bobSizes.length; j++) {
+                    if (bobSizes[j] == aliceSizes[i] + elDiff) {
+                        return new int[]{aliceSizes[i], bobSizes[j]};
+                    }
+                }
+            }
+        }
+        return new int[]{aliceSizes[temp], bobSizes[temp]};
+    }
+
+    public static int sumArrayElements(int[] array) {
+        int sum = 0;
+        for (Integer num : array) {
+            sum += num;
+        }
+        return sum;
     }
 }
