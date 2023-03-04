5109d7078390b3ce418d66029161362c41f88b42
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-04 14:54:19 +0300

    1011. Capacity To Ship Packages Within D Days - done (BAAAAD! binary - 5% memory)

--- Solution.java
+++ Solution.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] weights = {1, 2, 3, 4, 5, 6, 7, 8};
-        int days = 2;
+        int[] weights = {1, 2, 3, 1, 1};
+        int days = 4;
         System.out.println(shipWithinDays(weights, days));
     }
 
@@ -16,9 +16,24 @@ public class Solution {
         if (days == 1) {
             return maxCap;
         }
-        for (int i = minCap; i <= maxCap; i++) {
-            if (checkIfPassed(i, weights, days)) {
-                return i;
+        int mid;
+        int min = minCap;
+        int max = maxCap;
+
+        while (min <= max) {
+            if (checkIfPassed(min, weights, days)) {
+                return min;
+            }
+            if (checkIfPassed(max, weights, days) && !checkIfPassed(max - 1, weights, days)) {
+                return max;
+            }
+            mid = (min + max) / 2;
+            if (checkIfPassed(mid, weights, days) && !checkIfPassed(mid - 1, weights, days)) {
+                return mid;
+            } else if (checkIfPassed(mid, weights, days)) {
+                max = mid;
+            } else if (!checkIfPassed(mid, weights, days)) {
+                min = mid;
             }
         }
         return 0;
