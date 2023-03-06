84e30e97e28b8ef9c80492b31b1073136bfcf70c
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-07 02:29:17 +0300

    1122. Relative Sort Array - done

--- Solution.java
+++ Solution.java
@@ -1,25 +1,44 @@
+//1122. Relative Sort Array
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] arr1 = {2, 21, 43, 38, 0, 42, 33, 7, 24, 13, 12, 27, 12, 24, 5, 23, 29, 48, 30, 31};
+        int[] arr2 = {2, 42, 38, 0, 43, 21};
+        System.out.println(Arrays.toString(relativeSortArray(arr1, arr2)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] count = new int[1001];
+        for (int i = 0; i < arr1.length; i++) {
+            count[arr1[i]]++;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+
+        int[] result = new int[arr1.length];
+        int index = 0;
+
+        for (int i = 0; i < arr2.length; i++) {
+            int temp = count[arr2[i]];
+            while (temp > 0) {
+                result[index] = arr2[i];
+                temp--;
+                index++;
             }
+            count[arr2[i]] = 0;
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 0) {
+                int temp = count[i];
+                while (temp > 0) {
+                    result[index] = i;
+                    if (index == arr1.length - 1) {
+                        break;
+                    }
+                    index++;
+                    temp--;
+                }
+            }
         }
         return result;
     }
