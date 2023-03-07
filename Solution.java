7feaf2f3cb8b31d33ad1be17263970356c06480b
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-07 03:38:39 +0300

    1408. String Matching in an Array - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
+//1408. String Matching in an Array
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String[] words = {"leetcode", "et", "code"};
+        System.out.println(stringMatching(words));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static List<String> stringMatching(String[] words) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (!words[i].equals(words[j])) {
+                    if (words[i].contains(words[j])) {
+                        if (!list.contains(words[j])) {
+                            list.add(words[j]);
+                        }
+                    }
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return list;
     }
 }
