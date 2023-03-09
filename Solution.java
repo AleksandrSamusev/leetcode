d7ded37c132fc400298a4956c8a51a15bbcfe656
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-09 16:25:12 +0300

    1360. Number of Days Between Two Dates - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,95 @@
-import java.util.Arrays;
+//1360. Number of Days Between Two Dates
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String date1 = "2020-01-15";
+        String date2 = "2019-12-31";
+        System.out.println(daysBetweenDates(date1, date2));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int daysBetweenDates(String date1, String date2) {
+        String[] date1Splitted = date1.split("-");
+        String[] date2Splitted = date2.split("-");
+        int days1 = daysFromTheBeginningOfTheYear(date1Splitted);
+        int days2 = daysFromTheBeginningOfTheYear(date2Splitted);
+
+
+        long start = System.nanoTime();
+        for (int i = 1971; i < Integer.parseInt(date1Splitted[0]); i++) {
+            if (isLeapYear(i)) {
+                days1 += 366;
+            } else {
+                days1 += 365;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+
+        for (int i = 1971; i < Integer.parseInt(date2Splitted[0]); i++) {
+            if (isLeapYear(i)) {
+                days2 += 366;
+            } else {
+                days2 += 365;
+            }
         }
-        return result;
+        long end = System.nanoTime();
+        System.out.println(end - start);
+        return Math.abs(days1 - days2);
+    }
+
+    public static int daysFromTheBeginningOfTheYear(String[] year) {
+        int sum = 0;
+        switch (year[1]) {
+            case "02":
+                sum += 31;
+                break;
+
+            case "03":
+                sum += 59;
+                break;
+
+            case "04":
+                sum += 90;
+                break;
+
+            case "05":
+                sum += 120;
+                break;
+
+            case "06":
+                sum += 151;
+                break;
+
+            case "07":
+                sum += 181;
+                break;
+
+            case "08":
+                sum += 212;
+                break;
+
+            case "09":
+                sum += 243;
+                break;
+
+            case "10":
+                sum += 273;
+                break;
+
+            case "11":
+                sum += 304;
+                break;
+
+            case "12":
+                sum += 334;
+                break;
+        }
+        sum += Integer.parseInt(year[2]);
+        if (isLeapYear(Integer.parseInt(year[0])) && Integer.parseInt(year[1]) > 2) {
+            sum++;
+        }
+        return sum;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
