946117854bac3571b61c1393c0071497f591ecd6
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-09 17:03:16 +0300

    1207. Unique Number of Occurrences - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,30 @@
+//1207. Unique Number of Occurrences
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {1, 2, 2, 1, 1, 3};
+        System.out.println(uniqueOccurrences(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static boolean uniqueOccurrences(int[] arr) {
+        int[] array = new int[2001];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                array[Math.abs(arr[i])]++;
+            } else {
+                array[arr[i] + 1000]++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        Arrays.sort(array);
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] != 0) {
+                if (array[j] == array[j - 1]) {
+                    return false;
+                }
+            }
         }
-        return result;
+        return true;
     }
 }
