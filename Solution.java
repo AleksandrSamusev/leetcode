66ad4ffd3df6f1c64ea43b651263144030f95b8a
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-11 04:14:18 +0300

    1507. Reformat Date - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,56 @@
-import java.util.Arrays;
+//1507. Reformat Date
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String date = "20th Oct 2052";
+        System.out.println(reformatDate(date));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static String reformatDate(String date) {
+        String[] strArr = date.split(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(strArr[2]);
+        sb.append("-");
+        sb.append(LettersToNumbers(strArr[1]));
+        sb.append("-");
+        int i = 0;
+        if (Character.isDigit(strArr[0].charAt(1))) {
+            sb.append(strArr[0].charAt(0));
+            sb.append(strArr[0].charAt(1));
+        } else {
+            sb.append("0");
+            sb.append(strArr[0].charAt(0));
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
+        return sb.toString();
+    }
+
+    public static String LettersToNumbers(String s) {
+        switch (s) {
+            case "Jan":
+                return "01";
+            case "Feb":
+                return "02";
+            case "Mar":
+                return "03";
+            case "Apr":
+                return "04";
+            case "May":
+                return "05";
+            case "Jun":
+                return "06";
+            case "Jul":
+                return "07";
+            case "Aug":
+                return "08";
+            case "Sep":
+                return "09";
+            case "Oct":
+                return "10";
+            case "Nov":
+                return "11";
+            case "Dec":
+                return "12";
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return null;
     }
 }
