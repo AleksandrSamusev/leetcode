4807f5a88663b9a410d3a6d26f718a4e808efad7
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-12 04:10:09 +0300

    1619. Mean of Array After Removing Some Elements - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,21 @@
+//1619. Mean of Array After Removing Some Elements
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] arr = {6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5, 10, 8, 6, 6, 1, 0, 6, 10, 8, 2, 3, 4};
+        System.out.println(trimMean(arr));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int startIndex = arr.length / 20;
+        int endIndex = arr.length - 1 - arr.length / 20;
+        int sum = 0;
+        for (int i = startIndex; i <= endIndex; i++) {
+            sum += arr[i];
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return (double) sum / (arr.length - startIndex * 2);
     }
 }
