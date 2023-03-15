c33369c3e44ec24f5de42fd710c27462663cc147
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-15 18:11:55 +0300

    1128_Equivalent_of_number_domino_pairs - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,54 @@
+//1128_Equivalent_of_number_domino_pairs
+
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[][] dominoes = {{1, 1}, {2, 2}, {1, 1}, {1, 2}, {1, 2}, {1, 1}};
+        System.out.println(numEquivDominoPairs(dominoes));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int numEquivDominoPairs(int[][] dominoes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < dominoes.length; i++) {
+            if (!map.containsKey(pairAsString(dominoes[i])) && !map.containsKey(reversedPairAsString(dominoes[i]))) {
+                map.put(pairAsString(dominoes[i]), 1);
+            } else if (map.containsKey(pairAsString(dominoes[i])) && !map.containsKey(reversedPairAsString(dominoes[i]))) {
+                int value = map.get(pairAsString(dominoes[i]));
+                value++;
+                map.put(pairAsString(dominoes[i]), value);
+            } else if (map.containsKey(reversedPairAsString(dominoes[i])) && !map.containsKey(pairAsString(dominoes[i]))) {
+                int value = map.get(reversedPairAsString(dominoes[i]));
+                value++;
+                map.put(reversedPairAsString(dominoes[i]), value);
+            } else if (map.containsKey(pairAsString(dominoes[i])) && pairAsString(dominoes[i]).equals(reversedPairAsString(dominoes[i]))) {
+                int value = map.get(pairAsString(dominoes[i]));
+                value++;
+                map.put(pairAsString(dominoes[i]), value);
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+
+        int sum = 0;
+        for (Integer value : map.values()) {
+            if (value > 1) {
+                sum += value * (value - 1) / 2;
+            }
         }
-        return result;
+        return sum;
+
+    }
+
+    public static String pairAsString(int[] arr) {
+        return Arrays.toString(arr);
+    }
+
+    public static String reversedPairAsString(int[] arr) {
+        int[] pair = new int[arr.length];
+        int temp = arr[0];
+        pair[0] = arr[1];
+        pair[1] = temp;
+        return Arrays.toString(pair);
     }
 }
