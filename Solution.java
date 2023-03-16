d344fdb675f62a6f3da47dccf3b315c8c5f6e659
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-16 03:26:46 +0100

    2089. Find Target Indices After Sorting Array - done (no sort, 100%)

--- Solution.java
+++ Solution.java
@@ -1,7 +1,6 @@
 //2089. Find Target Indices After Sorting Array
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -12,16 +11,21 @@ public class Solution {
     }
 
     public static List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
+        int equal = 0;
+        int less = 0;
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > target) {
-                break;
+        for (Integer num : nums) {
+            if (num == target) {
+                equal++;
             }
-            if (nums[i] == target) {
-                list.add(i);
+            if (num < target) {
+                less++;
             }
         }
+        for (int i = less; i < less + equal; i++) {
+            list.add(i);
+        }
+
         return list;
     }
 }
