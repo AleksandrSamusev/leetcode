4b6c1e239f45cdc896f747253f4139cf5d5c3fef
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-16 21:05:25 +0100

    1935. Maximum Number of Words You Can Type - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,32 @@
-import java.util.Arrays;
+//1935. Maximum Number of Words You Can Type
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String text = "hello world";
+        String brokenLetters = "ad";
+        System.out.println(canBeTypedWords(text, brokenLetters));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static int canBeTypedWords(String text, String brokenLetters) {
+        int[] alphabet = new int[27];
+        for (int i = 0; i < brokenLetters.length(); i++) {
+            alphabet[brokenLetters.charAt(i) - 'a']++;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        String[] array = text.split(" ");
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            int localCount = 0;
+            for (int j = 0; j < array[i].length(); j++) {
+                if (alphabet[array[i].charAt(j) - 'a'] == 0) {
+                    localCount++;
+                } else {
+                    break;
+                }
+            }
+            if (localCount == array[i].length()) {
+                count++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return count;
     }
 }
