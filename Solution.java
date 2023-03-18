011ef00aca41970510fe37e873372cd0907e62fe
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-18 22:47:51 +0100

    2273. Find Resultant Array After Removing Anagrams - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,40 @@
+//2273. Find Resultant Array After Removing Anagrams
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String[] words = {"a", "b", "c", "d", "e"};
+        System.out.println(removeAnagrams(words));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static List<String> removeAnagrams(String[] words) {
+        List<String> list = new ArrayList<>();
+        list.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            if (!isAnagram(words[i - 1], words[i])) {
+                list.add(words[i]);
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+
+        return list;
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
         }
-        return result;
+        int[] array1 = new int[27];
+        int[] array2 = new int[27];
+        for (int i = 0; i < s1.length(); i++) {
+            array1[s1.charAt(i) - 'a']++;
+            array2[s2.charAt(i) - 'a']++;
+        }
+        if (!Arrays.equals(array1, array2)) {
+            return false;
+        }
+        return true;
     }
 }
