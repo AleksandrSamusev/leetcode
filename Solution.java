ad9de621255ece84ff9d514aa492b9656c470bee
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-18 19:11:30 +0100

    1957. Delete Characters to Make Fancy String - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,25 @@
-import java.util.Arrays;
+//1957. Delete Characters to Make Fancy String
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "aaabaaaa";
+        System.out.println(makeFancyString(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static String makeFancyString(String s) {
+        if (s.length() == 1 || s.length() == 2) {
+            return s;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        sb.append(s.charAt(1));
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == s.charAt(i - 2)) {
+                continue;
+            } else {
+                sb.append(s.charAt(i));
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return sb.toString();
     }
 }
