34ebe33894ad49ad50371e29d0093cfc13bd8efb
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-19 01:23:22 +0100

    2303. Calculate Amount Paid in Taxes - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,36 @@
-import java.util.Arrays;
+//2303. Calculate Amount Paid in Taxes
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[][] brackets = {{4, 8}, {5, 49}};
+        int income = 2;
+        System.out.println(calculateTax(brackets, income));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static double calculateTax(int[][] brackets, int income) {
+        int rest = 0;
+        double taxAmount = 0;
+        if (income == 0) {
+            return 0.0;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        for (int i = 0; i < brackets.length; i++) {
+            if (brackets[i][0] <= income) {
+                if (i == 0) {
+                    taxAmount += brackets[i][0] * brackets[i][1] / 100.0;
+                } else {
+                    taxAmount += (brackets[i][0] - brackets[i - 1][0]) * brackets[i][1] / 100.0;
+                }
+            } else {
+                if (i != 0) {
+                    rest = income - brackets[i - 1][0];
+                    taxAmount += (rest * brackets[i][1] / 100.0);
+                    return taxAmount;
+                } else {
+                    taxAmount += income * brackets[i][1] / 100.0;
+                    return taxAmount;
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return taxAmount;
     }
 }
