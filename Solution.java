dfa39108f51e916600deb3bfef5efa85772c7dcf
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-19 03:17:19 +0100

    1260. Shift 2D Grid - done

--- Solution.java
+++ Solution.java
@@ -1,25 +1,42 @@
-import java.util.Arrays;
+//1260. Shift 2D Grid
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[][] grid = {{3, 8, 1, 9}, {19, 7, 2, 5}, {4, 6, 11, 10}, {12, 0, 21, 13}};
+        int k = 4;
+        System.out.println(shiftGrid(grid, k));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] lastColumn = new int[m];
+        while (k > 0) {
+            k--;
+            for (int i = 0; i < lastColumn.length; i++) {
+                lastColumn[i] = grid[i][n - 1];
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = n - 1; j > 0; j--) {
+                    grid[i][j] = grid[i][j - 1];
+                }
+            }
+            grid[0][0] = lastColumn[lastColumn.length - 1];
+
+            for (int i = 1; i < m; i++) {
+                grid[i][0] = lastColumn[i - 1];
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        for (int i = 0; i < m; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                temp.add(grid[i][j]);
+            }
+            result.add(temp);
         }
         return result;
     }
