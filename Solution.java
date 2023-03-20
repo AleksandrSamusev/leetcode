849b0cb845f3fa565a546354a34f3327b9e06d72
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-20 04:06:51 +0100

    605. Can Place Flowers - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,57 @@
-import java.util.Arrays;
+//605. Can Place Flowers
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] flowerbed = {0, 1, 0};
+        int n = 1;
+        System.out.println(canPlaceFlowers(flowerbed, n));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int notZeros = 0;
+        int zeroIndex = -1;
+        int zeros = 0;
+        int count = 0;
+
+        if (flowerbed.length == 1 && flowerbed[0] == 1 && n == 0) {
+            return true;
+        } else if (flowerbed.length == 1 && flowerbed[0] == 1 && n == 1) {
+            return false;
+        } else if (flowerbed.length == 1 && flowerbed[0] == 0 && n == 1) {
+            return true;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        if (flowerbed[0] == 0) {
+            zeroIndex = 0;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                zeros++;
+                if (i == flowerbed.length - 1 && notZeros == 0) {
+                    if (flowerbed.length % 2 == 0) {
+                        count = flowerbed.length / 2;
+                    } else {
+                        count = flowerbed.length / 2 + 1;
+                    }
+                    return count >= n;
+                } else if (i == flowerbed.length - 1 && notZeros != 0) {
+                    count += zeros / 2;
+                    return count >= n;
+                }
+            } else {
+                notZeros++;
+                if (zeroIndex == 0 && zeros >= 2) {
+                    count += zeros / 2;
+                    zeros = 0;
+                    zeroIndex = -1;
+                } else if (zeros >= 3 && zeros % 2 != 0) {
+                    count += zeros / 2;
+                } else if (zeros >= 3) {
+                    count += zeros / 2 - 1;
+                }
+                zeros = 0;
             }
+
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return count >= n;
     }
 }
