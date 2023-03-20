24637ea4efd6155e6eb0b00392cef9d8644329b8
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-20 20:01:35 +0100

    1304. Find N Unique Integers Sum up to Zero - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,36 @@
+//1304. Find N Unique Integers Sum up to Zero
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int n = 1;
+        System.out.println(Arrays.toString(sumZero(n)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int[] sumZero(int n) {
+        int[] result = new int[n];
+        int temp = 1;
+
+        if (n % 2 == 0) {
+            for (int i = 0; i < n / 2; i++) {
+                result[i] = -n / 2 + i;
+            }
+            for (int i = n / 2; i < result.length; i++) {
+                result[i] = temp;
+                temp++;
+            }
+        } else {
+            n--;
+            for (int i = 0; i < n / 2; i++) {
+                result[i] = -n / 2 + i;
+            }
+            for (int i = n / 2 + 1; i < result.length; i++) {
+                result[i] = temp;
+                temp++;
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
         return result;
+
     }
 }
