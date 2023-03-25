52497e664a119eadec711f02904d3306cb81c3c3
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-25 16:00:14 +0100

    1025. Divisor Game - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,15 @@
-import java.util.Arrays;
+//1025. Divisor Game
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int n = 3;
+        System.out.println(divisorGame(n));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static boolean divisorGame(int n) {
+        if (n % 2 != 0) {
+            return false;
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return true;
     }
 }
