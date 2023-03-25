9161a1074ded26f0aa0be0cbceb7120e92b41543
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-25 14:40:35 +0100

    414. Third Maximum Number - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,30 @@
+//414. Third Maximum Number
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {2, 2, 3, 1};
+        System.out.println(thirdMax(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
+    public static int thirdMax(int[] nums) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+        int initMax = nums[nums.length - 1];
+        int count = 1;
+        int max;
+        if (nums.length <= 2) {
+            return nums[nums.length - 1];
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                max = nums[i];
+                count++;
+                if (count == 3) {
+                    return max;
+                }
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return initMax;
     }
 }
