9b9339f6f6c4fde35df15225c56dfee765288901
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-25 16:01:28 +0100

    1025. Divisor Game - done

--- Solution.java
+++ Solution.java
@@ -2,14 +2,11 @@
 
 public class Solution {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 4;
         System.out.println(divisorGame(n));
     }
 
     public static boolean divisorGame(int n) {
-        if (n % 2 != 0) {
-            return false;
-        }
-        return true;
+        return n % 2 == 0;
     }
 }
