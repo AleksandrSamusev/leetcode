325b692026af01a51f97a49115549b88b21845d4
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-25 14:25:42 +0100

    961. N-Repeated Element in Size 2N Array - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,19 @@
-import java.util.Arrays;
+//961. N-Repeated Element in Size 2N Array
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {5, 1, 5, 2, 5, 3, 5, 4};
+        System.out.println(repeatedNTimes(nums));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static int repeatedNTimes(int[] nums) {
+        int[] arr = new int[10001];
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]]++;
+            if (arr[nums[i]] == nums.length / 2) {
+                return nums[i];
             }
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return 0;
     }
 }
