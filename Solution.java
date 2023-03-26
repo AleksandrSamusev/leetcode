fefa91e69939ccef9aa9a2dfdd56dd95f3e260b7
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-26 01:46:53 +0100

    1309. Decrypt String from Alphabet to Integer Mapping - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,55 @@
-import java.util.Arrays;
+//1309. Decrypt String from Alphabet to Integer Mapping
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "10#11#12";
+        System.out.println(freqAlphabets(s));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static String freqAlphabets(String s) {
+        char[] letters = {'0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        StringBuilder result = new StringBuilder();
+        boolean flag = s.charAt(s.length() - 1) == '#';
+        String[] str = s.split("#");
+        if (str.length == 1 && flag) {
+            return numbersToWord(str[0], letters);
+        } else if (str.length == 1) {
+            return numbersToWordNoFlag(str[0], letters);
+        } else if (str.length > 1 && flag) {
+            for (String value : str) {
+                result.append(numbersToWord(value, letters));
             }
+            return result.toString();
+        } else if (str.length > 1) {
+            for (int i = 0; i < str.length - 1; i++) {
+                result.append(numbersToWord(str[i], letters));
+            }
+            result.append(numbersToWordNoFlag(str[str.length - 1], letters));
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+        return result.toString();
+    }
+
+    public static String numbersToWord(String s, char[] letters) {
+        StringBuilder sb = new StringBuilder();
+        if (s.length() > 2) {
+            int i = 0;
+            while (i < s.length() - 2) {
+                sb.append(letters[Integer.parseInt(String.valueOf(s.charAt(i)))]);
+                i++;
+            }
+            sb.append(letters[Integer.parseInt(s.substring(s.length() - 2))]);
+        } else {
+            sb.append(letters[Integer.parseInt(s)]);
         }
-        return result;
+        return sb.toString();
+    }
+
+    public static String numbersToWordNoFlag(String s, char[] letters) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(letters[Integer.parseInt(String.valueOf(s.charAt(i)))]);
+        }
+        return sb.toString();
     }
 }
