0cd418ec224c2d41727f728834d3b055bd89d6ef
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-26 03:15:04 +0200

    1365. How Many Numbers Are Smaller Than the Current Number - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,39 @@
+//1365. How Many Numbers Are Smaller Than the Current Number
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] nums = {8, 1, 2, 2, 3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] sortedArray = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortedArray);
+        int[] result = new int[nums.length];
+
+        if (nums.length == 1) {
+            return new int[]{0};
         }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
-            }
-        }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = findFirstOccurrence(sortedArray, nums[i]);
         }
         return result;
+    }
+
+    public static int findFirstOccurrence(int[] array, int num) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (num > array[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 }
