b181d73d66c9c225893cecab1fba484d76134b62
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-03-27 15:58:49 +0200

    205. Isomorphic Strings - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,24 @@
-import java.util.Arrays;
+//205. Isomorphic Strings
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        String s = "foo";
+        String t = "bar";
+        System.out.println(isIsomorphic(s, t));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)) && !map.get(s.charAt(i)).equals(t.charAt(i)) ||
+                    !map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) {
+                return false;
             }
+            map.put(s.charAt(i), t.charAt(i));
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return true;
     }
 }
