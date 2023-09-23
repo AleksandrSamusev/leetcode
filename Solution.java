6761b01bfaf491417970e8fa47876678354eb209
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-09-23 18:55:58 +0200

    1491 - done

--- Solution.java
+++ Solution.java
@@ -1,26 +1,23 @@
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {7,7,6,10,6,5,5,8,8,9,9,11,11};
-        System.out.println(singleNumber(nums));
+        int[] salary = {7, 7, 6, 10, 6, 5, 5, 8, 8, 9, 9, 11, 11};
+        System.out.println(average(salary));
     }
 
-    public static int singleNumber(int[] nums) {
-        var result = 0;
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        if(nums[0] != nums[1]) {
-            result = nums[0];
-        }
-        for(int i = 1; i<nums.length-2; i++) {
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
-                result = nums[i];
+    public static double average(int[] salary) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < salary.length; i++) {
+            if (salary[i] >= max) {
+                max = salary[i];
             }
+            if (salary[i] < min) {
+                min = salary[i];
+            }
+            sum += salary[i];
         }
-        if(nums[nums.length-1] != nums[nums.length-2]) {
-            result = nums[nums.length-1];
-        }
-        return result;
+        return (double) (sum - max - min) / (double) (salary.length - 2);
     }
 }
